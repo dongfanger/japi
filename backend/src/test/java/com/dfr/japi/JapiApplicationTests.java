74683af7f5ef8657f08b0e6cb3e839b45276e812
wanggang424 <wanggang424@jd.com>
2022-06-14 12:51:48 +0800

    项目初始化

---
+++ backend/src/test/java/com/dfr/japi/JapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfr.japi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
